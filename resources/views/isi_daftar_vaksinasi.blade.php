0d232bd11a2f6367b9ca4217d7d3d775c720b336
akirareka <akira.jiraiya@gmail.com>
2021-12-07 16:49:45 +0700

    Penambahan Klinik

--- resources/views/isi_daftar_vaksinasi.blade.php
+++ resources/views/isi_daftar_vaksinasi.blade.php
@@ -80,9 +80,12 @@
                             <label for="lokasi">Lokasi</label>
                             <select class="form-select" aria-label="Default select example" style="font-family:Montserrat; color:#70ccb4" name="tempat_vaksin">
                                 <option selected>Pilih Klinik</option>
-                                <option value="1" >Klinik 1</option>
-                                <option value="2">Klinik 2</option>
-                                <option value="3">Klinik 3</option>
+                                <option value="1" >Klinik Utama Perisai Husada</option>
+                                <option value="2">Klinik Utama Melati Bunda</option>
+                                <option value="3">Klinik Utama Medika Antapani</option>
+                                <option value="4">Klinik Utama Netra</option>
+                                <option value="5">Klinik Utama Monalisa</option>
+                                <option value="6">Klinik Utama Jiwa GRHA Atma</option>
                               </select>
                         </div>
     
